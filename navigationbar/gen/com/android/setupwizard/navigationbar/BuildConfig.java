0239e329656da3465a80906849751e429cdd1d72
tomorrow_p <tomorrow_p@163.com>
2017-03-30 10:51:15 +0800

    init cm settings

---
+++ navigationbar/gen/com/android/setupwizard/navigationbar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.setupwizard.navigationbar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
